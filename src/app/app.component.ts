e8436638e2724e7358ceec4c9b2af790b503b550
David Hale <david.hale7@gmail.com>
2017-11-04 14:19:22 -0700

    scroll npm working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PageScrollConfig } from 'ng2-page-scroll';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {
+    PageScrollConfig.defaultScrollOffset = 70;
+    PageScrollConfig.defaultDuration = 1000;
+    PageScrollConfig.defaultEasingLogic = {
+      ease: (t: number, b: number, c: number, d: number): number => {
+          // easeInOutExpo easing
+          if (t === 0) return b;
+          if (t === d) return b + c;
+          if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+          return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+      }
+    };
+  };
   title = 'Portfolio';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
